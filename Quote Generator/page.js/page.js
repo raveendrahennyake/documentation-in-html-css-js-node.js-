049ef5e4b735bf14b166c1297dd6fc4daed10c78
raveendrahennyake <raveendralakshan5@gmail.com>
2024-03-24 21:05:58 +0530

    message

--- Quote Generator/page.js/page.js
+++ Quote Generator/page.js/page.js
@@ -1,16 +1,28 @@
 
-const api_url = "https://api.quotable.io/random";
+document.addEventListener("DOMContentLoaded", function() {
+    const api_url = "https://api.quotable.io/random";
 
-const quote=document.getElementById("quote");
-const author=document.getElementById("author");
+    const quote = document.getElementById("quote");
+    const author = document.getElementById("author");
+    const button3 = document.getElementById("button3");
+    
 
-async function  getquote (url) {
-    const response=await fetch (url);
-    var data= await response.json();
-    console.log(data);
+    async function getquote(url) {
+        const response = await fetch(url);
+        var data = await response.json();
+        console.log(data);
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+        console.log(quote);
 
-  }
-   getquote (api_url);
+    }
+
+    button3.onclick = function() {
+        getquote(api_url);
+    };
+    
+    getquote(api_url);
+});
 
  
 
