6e774885ae84436e991e56f801f45bab9e3f66a9
raveendrahennyake <raveendralakshan5@gmail.com>
2024-04-03 16:39:02 +0530

    message

--- methods/settime/set.js
+++ methods/settime/set.js
@@ -5,16 +5,29 @@ function getname (){
     time.textContent=a;
 }
 //set interver
-let b=setInterval(getname,1000);
+//let b=setInterval(getname,1000);
 
 //clear intervel
 function clearTime (){
     clearInterval(b);
 }
 stoptime.addEventListener("click",clearTime);{
-    
+
 
 }
+
+//set time out
+
+function myfunction () {
+    alert("Hello");
+}
+
+function settimeout() {
+    setTimeout(myfunction,3000);
+
+}
+btn.addEventListener("click",settimeout); 
+
 
 
 
