a984f2e48271cfe2089f38fed49dca55725b981c
Raveendra Hennyake <157197517+raveendrahennyake@users.noreply.github.com>
2024-04-30 19:23:19 +0530

    Create page1.js

---
+++ page1.js
@@ -0,0 +1,259 @@
+// let firstname="Lakshan";
+// let age=25;
+// let  student=false
+
+// // document.getElementById(Text1).textContent=firstname;
+// document.getElementById("Text1").innerText=`${firstname}`;
+// document.getElementById("Text2").innerText=`${age}`;
+// document.getElementById("Text3").innerText=`${student}`;
+
+//Aricmatic Opertaors
+
+
+// let username;
+// username=window.prompt("My name is a ")
+// console.log(username);
+
+
+// let logname;
+// logname=window.prompt("my log name is a ");
+// console.log(logname);
+
+
+// const sumbit=document.getElementById("sumbit1");
+
+// sumbit.addEventListener("click",function () {
+//     const input=document.getElementById("inputtext");
+//     console.log(input);
+//     // inputtext.textContent=`${input}`;
+//     // console.log(inputtext);
+
+// });
+
+
+// const getname=document.getElementById("inputtext");
+// console.log(getname)
+
+// console.log(savename)
+
+
+// const setbutton=document.getElementById("sumbit1");
+
+// setbutton.addEventListener("click",function() {
+//     const inputs=document.getElementById("text1");
+//     inputs.textContent=text1;
+
+// });
+
+// document.getElementById("sumbit1").addEventListener("click",function () {
+//     const givename=document.getElementById("text1");
+//     text1.textContent=`${givename}`
+
+
+// });
+
+const setbutton=document.getElementById("sumbit1");
+
+// setbutton.onclick=function () {
+//     var usernames=document.getElementById("text1").value;
+//     let hellos=document.getElementById("text5");
+//     hellos.textContent=usernames;
+
+// };
+// setbutton.addEventListener("click",function (){
+//     const savenamess=document.getElementById("text1").value;
+//     text5.textContent=savenamess;
+    
+// });
+
+
+// let USERNAME;
+
+// USERNAME=document.getElementById("")
+
+let age;
+// age=window.prompt("input your age is ");
+// console.log(age);
+
+
+// button1=document.getElementById("submit8")
+// button1.onclick=function () {
+//     age=window.prompt("input your age is ");
+//     console.log(age);
+// }
+
+//Type conversions
+
+//change datatype of (string/number/booleans)
+
+
+// let ages=window.prompt("HOW OLD ARE YOU ${age}")
+
+// ages+=1;
+
+// console.log(ages);
+// let num;
+
+// button3.onclick=function () {
+//     let ages=window.prompt("HOW OLD ARE YOU ")
+//    ages+=1;
+
+//    console.log(ages);
+// }
+ 
+// button=document.getElementById("button3");
+
+
+// button.onclick=function () {
+//     username=window.prompt("Enter your age is ")
+//     username=Boolean(username)
+//     // username+=1
+//     console.log(username)
+// }
+
+
+let firstname="Apple";
+let Secondname="Banna";
+let thirdname="rice";
+
+
+firstname=Number (firstname);
+Secondname=String (Secondname);
+thirdname=Boolean (thirdname);
+// 
+// console.log(firstname+Secondname+thirdname)
+
+// let sum=firstname+Secondname+thirdname
+
+// console.log(`SUM IS A ${sum}`);
+
+
+// console.log(`${firstname}${Secondname}${thirdname}`)
+
+// console.log({firstname}{Secondname}{thirdname});
+
+
+// let userNamesss="Lakshan";
+
+// // console.log("MY name is "+userNamesss )
+
+// console.log(`MY name is a ${userNamesss}`)
+
+
+let PIE;
+let radies;
+let Circulemeter;
+
+// PIE=3.14;
+// radies=window.prompt("Enter your radies")
+// radies=Number(radies);
+// Circulemeter=2*PIE*radies
+//  console.log(Circulemeter)
+
+
+// let sumbites=document.getElementById("submit87");
+
+// sumbites.onclick=function() {
+//     let radies=document.getElementById("text0").value;
+//     radies=Number(radies);
+//      console.log(radies);
+//     PIE=3.14;
+//     Circulemeter=2*PIE*radies
+//     let sumers=document.getElementById("ENTER");
+//     sumers.textContent=Circulemeter;
+//     // console.log(Circulemeter);
+// }
+
+
+//Counter Number
+let count=0;
+
+//Increase Button
+
+
+// let button1=document.getElementById("button1");
+// let button2=document.getElementById("button2");
+// let button3=document.getElementById("button3");
+// button1.addEventListener("click",function () {
+//    count=count+1
+//     let place=document.getElementById("text1");
+//     place.textContent=count;
+//     // console.log(sum);
+// });
+
+// button2.addEventListener("click",function () {
+//    count=count-1
+//     let place=document.getElementById("text1");
+//     place.textContent=count;
+   
+// });
+
+
+// button3.addEventListener("click",function () {
+//     let place=document.getElementById("text1");
+//     count=0;
+//     place.textContent=count;
+
+    
+    
+// });
+
+// math function 
+
+// const arr=[12,88,66,55,11,48];
+
+// for (let i=0;i<arr.length;i+=1){
+//     console.log(arr[i]);
+// }
+
+
+// let x=10;
+// let y=15;
+// let z=1000;
+
+
+// let sum=Math.max(x,y,z);
+// console.log(sum);
+
+
+//Random number Gernerate
+
+// let randomnumber=Math.floor(Math.random());
+// console.log(randomnumber);
+
+// let save=document.getElementById("text1");
+// save.textContent=randomnumber
+
+
+// let button=document.getElementById("button1");
+
+
+// button.addEventListener("click",function () {
+//     let random=Math.floor(Math.random()*6);
+//     let show=document.getElementById("text1");
+//     show.textContent=random
+
+
+// });
+
+
+//conditional retirinus
+//(if/else/elif)
+
+
+// let number=15;
+// if (number>0 && number<10){
+//     console.log("its is a first number")
+// }
+
+// else if (number>10 && number<20){
+//     console.log("It is a second ")
+// }
+
+// else if (number>20 && number<50){
+//     console.log("It is a third ")
+// }
+
+// else  {
+//     console.log("It is a large ")
+// }
