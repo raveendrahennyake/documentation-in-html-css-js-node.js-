872a90a2e2f5df938644cd9d8fa5d493628d55b1
Raveendra Hennyake <157197517+raveendrahennyake@users.noreply.github.com>
2024-11-30 17:21:48 +0530

    Update exceptions.java

--- exceptions.java
+++ exceptions.java
@@ -88,5 +88,24 @@ System.out.println("After multi-catch.");
 }
 }
 
+  // Demonstrate finally.
+class FinallyDemo {
+// Throw an exception out of the method.
+static void procA() {
+try {
+System.out.println("inside procA");
+throw new RuntimeException("demo");
+} finally {
+System.out.println("procA's finally");
+}
+}
+// Return from within a try block.
+static void procB() {
+try {
+System.out.println("inside procB");
+return;
+} finally {
+System.out.println("procB's finally");
+
 
 
